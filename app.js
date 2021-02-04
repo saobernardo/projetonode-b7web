22f8ded6fa5a624fb6ea661a2ae12dfaac14b97c
dm10fit <bernardo.pinheiro@hotmail.com>
2021-02-04 09:09:36 -0300

    Installed and configured templates

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const mustache = require('mustache-express');
 const mainRoutes = require("./routes/index");
+const helper = require('./helpers');
 
 //Configurações
-
 const app = express();
+//antes do router, usa-se o helper
+app.use((req, res, next)=>{
+    res.locals.h = helper;
+    res.locals.teste='123';
+    next();
+});
 app.use('/', mainRoutes);
-//app.use('/admin', adminRoutes);
-
 app.use(express.json());
+
+//configurando Template Engine
+app.engine('mst', mustache(__dirname+'/views/partials', '.mst'));
+app.set('view engine', 'mst');
+app.set('views', __dirname + '/views');
 
 //exportando a app
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,11 @@ URL PARAMETER: request.params
 */
 
 routes.get('/', (request, response)=>{
-    response.json(request.query);
-});
-
-routes.get('/posts/:id', (request, response)=>{
-    id = request.params.id;
-    response.send("ID do post: "+id);
-});
-
-routes.get('/sobre', (request, response)=>{
-    response.send("Hello World 2");
+    let obj = {
+        title: "Título de teste",
+        pageTitle: "Título da página"
+    };
+    response.render('home', obj);
 });
 
 module.exports = routes;
